03be9dc21e37b192272d866470861efac8b740d2
Steve Parker <stevepkr84@gmail.com>
2026-03-01 17:54:15 +0000

    apply gitignore

--- moveZip.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-for %%a in ("%~dp0\.") do set "modname=%%~nxa"
-rmdir /s /q "%USERPROFILE%\Documents\My Games\FarmingSimulator2025\mods\%modname%\"
-move /y "%modname%.zip" "%USERPROFILE%\Documents\My Games\FarmingSimulator2025\mods\"
--- package.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-for %%a in ("%~dp0\.") do set "modname=%%~nxa"
-
-del %modname%.zip
-7z a -tzip %modname%.zip -w . -x!*.git* -x!*.bat -x!*.vscode* -x!*.md -x!*.kiro* -x!*refs*
